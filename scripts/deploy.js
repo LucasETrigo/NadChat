6acaf28257e0f3fd6810a1aed39cba1adfae396d
Lucas <92489915+LucasETrigo@users.noreply.github.com>
2025-03-06 20:10:54 -0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,1 @@
-import { ethers } from 'hardhat';
 
-async function main() {
-    const NadChat = await ethers.getContractFactory('NadChat');
-    const nadChat = await NadChat.deploy();
-    await nadChat.waitForDeployment();
-    console.log('NadChat deployed to:', nadChat.target);
-}
-
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
